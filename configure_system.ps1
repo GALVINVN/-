d5f5f778f9d9616a1390d6c228e9d2d9d7a5ab5e
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-06-06 07:38:03 +0700

    configure_system.ps1

--- configure_system.ps1
+++ configure_system.ps1
@@ -1,5 +1,3 @@
-Stop-Service -Name WSearch -Force
-Stop-Service -Name WinDefend -Force
 Set-MpPreference -PUAProtection 0
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name 'PUAProtection' -Value 0
 Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\*" -Force
@@ -71,4 +69,5 @@ Get-ScheduledTask | Where-Object {$_.TaskPath -like "\Microsoft\Windows\WindowsU
 Get-ScheduledTask | Where-Object {$_.TaskName -like "*reboot*" -or $_.TaskName -like "*restart*"} | ForEach-Object {Unregister-ScheduledTask -TaskName $_.TaskName -Confirm:$false}
 Get-ScheduledTask | ForEach-Object {Unregister-ScheduledTask -TaskName $_.TaskName -TaskPath $_.TaskPath -Confirm:$false}
 Clear-RecycleBin -Force
+powershell.exe -ExecutionPolicy Bypass -File C:\Users\Public\Downloads\LFO.ps1
 exit
